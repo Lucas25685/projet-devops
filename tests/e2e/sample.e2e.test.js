9b525cd242128ac3092f89cdc12ce11ee9d43d88
ThibaultDiguet <84134140+ThibaultDiguet@users.noreply.github.com>
2025-07-11 11:15:29 +0200

    fix

--- tests/e2e/sample.e2e.test.js
+++ tests/e2e/sample.e2e.test.js
@@ -6,9 +6,3 @@ test('GET / returns 200', async () => {
   const res = await request(app).get('/');
   expect(res.statusCode).toBe(200);
 });
-
-afterAll(async () => {
-  //délai pour être sur que tout ce qui est en arriere plan est terminé
-  await new Promise((res) => setTimeout(res, 100));
-  await pool.end();
-});
