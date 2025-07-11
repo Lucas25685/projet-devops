e8e677141d5930229ac0c6bc424bf79e30d7b3e3
ThibaultDiguet <84134140+ThibaultDiguet@users.noreply.github.com>
2025-07-11 11:22:05 +0200

    fix

--- tests/e2e/sample.e2e.test.js
+++ tests/e2e/sample.e2e.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const {pool} = require('../../src/db');
 
 test('GET / returns 200', async () => {
   const res = await request(app).get('/');
   expect(res.statusCode).toBe(200);
+});
+
+afterAll(async () => {
+  await pool.end();
 });
